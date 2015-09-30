133d09eed691fdea91766382691bc6f5fb978adc
mingwailau <MingWai@Mings-MacBook-Air.local>
2015-09-30 13:42:58 -0400

    Initial Commit

---
+++ CameraSocialEmailApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CameraSocialEmailApp
+//
+//  Created by Ming Wai on 30/9/15.
+//  Copyright (c) 2015 com.Ming Wai. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
